77b9c0c1a8193a5da4e1e94db6c9a5684e8dfa59
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-17 16:40:53 +0100

    Primer commit

---
+++ src/EntregaM1Fase1App.java
@@ -0,0 +1,19 @@
+
+
+
+/**
+ * @author stefano
+ *
+ */
+
+public class EntregaM1Fase1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String nom = "Pau", cognom1 = "Garcia", cognom2 = "Perez";
+	int dia=17, mes=7, any=1999;
+	System.out.println( cognom1 + cognom2+"," + nom);
+	System.out.println( dia+"/"+mes+"/"+any);
+	}
+
+}
