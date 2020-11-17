fa37d9a3127bf2ffd6b5ce47ab44185c3bc5fd8a
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-17 18:34:11 +0100

    primer commit

---
+++ src/EntregaM1Fase2App.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * @author stefano
+ *
+ */
+public class EntregaM1Fase2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int ANY=1948;
+		final int  FREQANYSTRASPAS=4;
+		int anyNeixament=2003;
+		int resultat=(anyNeixament-ANY)/FREQANYSTRASPAS;
+		System.out.println(resultat);		
+
+	}
+
+}
